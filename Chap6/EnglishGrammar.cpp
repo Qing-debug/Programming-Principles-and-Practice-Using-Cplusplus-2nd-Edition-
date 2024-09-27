e5e85cee875cd099254744eebfaac079ce40172d
Qing-debug <93039646+Qing-debug@users.noreply.github.com>
2024-09-27 21:07:15 +0800

    Add files via upload

---
+++ Chap6/EnglishGrammar.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//known issue: doing std::cin.get() twice works for everything aside from the nmoun func since we expect the first char to be a character and not whitespace, unlike the rest of the input.
+
+constexpr int valid_nouns_count = 3;
+constexpr int valid_verbs_count = 3;
+constexpr int valid_conjuncs_count= 3;
+
+std::vector<std::string> vec_noun = { "birds", "fish", "C++" };
+std::vector<std::string> vec_verb = { "rules", "fly", "swim" };
+std::vector<std::string> vec_conjuncs = { "and", "or", "but" };
+
+void resetCinn() {
+	std::cin.clear(); //reset cin if in error state so it can be used in the future
+}
+
+void clearInputStream() {
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignores entire input stream or until the delimiter is met '\n' (also discards delimiter from input buffer)
+}
+
+bool isNoun(std::string word) {
+	for (auto it = vec_noun.begin(); it != vec_noun.end(); ++it) {
+		if (word == *it) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
+bool isVerb(std::string word) {
+
+	for (auto it = vec_verb.begin(); it != vec_verb.end(); ++it) {
+		if (word == *it) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool isConjunc(std::string word) {
+
+	for (auto it = vec_conjuncs.begin(); it != vec_conjuncs.end(); ++it) {
+		if (word == *it) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+
+bool isFullstop() {
+	char ch = std::cin.get();
+	if (ch == '.') {
+		return true;
+	}
+	else {
+		std::cin.putback(ch);
+		return false;
+	}
+}
+
+bool sentence();
+
+bool conjunction() {
+	char ch = std::cin.get();
+	if (ch == ' ') {
+		return false;
+	}
+	std::cin.putback(ch);
+	std::string potential_conjunc;
+	std::cin >> potential_conjunc;
+	std::cin.get(); //consume whitespace after reading in string
+
+	if (isConjunc(potential_conjunc)) {
+		return sentence();
+	}
+	else {
+		return false;
+	}
+
+
+}
+
+//structure of sentence : noun verb 
+//						: sentence conjunction sentence
+bool sentence() {
+	char ch;
+	ch = std::cin.get();
+	if (ch == ' ') {
+		return false;
+	}
+	std::cin.putback(ch);
+	std::string potential_noun;
+	std::cin >> potential_noun; 
+	//should this next part be nested? Not sure yet.
+	if (!isNoun(potential_noun)) {
+		return false;
+	}
+	std::cin.get(); //consume whitespace after reading in string
+	ch = std::cin.get();
+	if (ch == ' ') {
+		return false;
+	}
+	std::cin.putback(ch);
+	std::string potential_verb;
+	std::cin >> potential_verb;
+	if (!isVerb(potential_verb)) {
+		return false;
+	}
+	std::cin.get();//consume whitespace after reading in string
+	if (isFullstop()) {
+		return true;
+	}
+	return conjunction();
+
+}
+
+//validate this works for a plethora of input.
+int main() {
+
+	while (true) {
+		std::cout << "input:";
+		bool result = sentence() << '\n';
+		if (result) {
+			std::cout << "OK" << '\n' << '\n';
+		}
+		else {
+			std::cout << "Not OK" << '\n' << '\n';
+		}
+		clearInputStream();
+	}
+}
